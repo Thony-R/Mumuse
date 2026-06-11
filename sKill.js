73069536b49e753b50b332a3af532e66cd90a88f
Thony <anthony.rita83@gmail.com>
2026-06-11 15:38:36 +0200

    modif pas au point

--- sKill.js
+++ sKill.js
@@ -1,13 +1,53 @@
-let score =100;
+let scoreM =2700;
+let score =2700;
+let vie = 400;
+
 const life =document.querySelector("#score");
 document.querySelector("#img").addEventListener('mousedown',function(event){
     
     if(event.button ===0)
     {
-        score--;
-        // life.textContent =score;
-        
-document.getElementById('vie').style.width = '${score}px';
+        let degats =degat();
+        score-=degats;
+        vie =((score*400)/scoreM);
+        console.log(degats);
+        life.textContent =score;
+        document.getElementById('vie').style.width = `${vie}px`;
+        if(score<=(scoreM/2))
+        {
+            document.getElementById('vie').style.backgroundColor = 'orange';
+        }
+        if(score<=scoreM/4)
+        {
+            document.getElementById('vie').style.backgroundColor = 'red';
+        }
     }
 });
 life.textContent =score;
+
+
+
+ function getrandom(max)
+{
+    return Math.floor(Math.random()*max);
+}
+function degat()
+{
+    if(getrandom(100)>4)
+    {
+        return (getrandom(14)+1);
+    }
+    else
+    {
+        return 65;
+    }
+}
+
+fetch('https://pokeapi.co/api/v2/pokemon/'+getrandom(151))
+.then(reponse => reponse.json())
+.then(resultat => {
+    ecran(resultat)
+});
+function ecran(pok){
+    document.getElementById('img').src = pok.sprites.other.dream_world.front_default;
+}
